ccd86c74e3951cdbafea69ad3bdb78dbd6bdb798
tvgDev <thiago.guimaraaes.dev@gmail.com>
2025-10-15 11:12:49 -0300

    fix: colors and background

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -27,7 +27,7 @@ const Index = () => {
           </div>
         </section>
 
-        <div className="bg-[url(./src/assets/Background-black.jpg)] bg-cover bg-center transform -skew-y-3 -translate-y-4">
+        <div className="bg-[url(./src/assets/Background-blackV2.png)] bg-cover bg-center transform -skew-y-3 -translate-y-4">
           <section className="py-20 skew-y-3">
             <TestimonialsSection />
             <SocialSection />
